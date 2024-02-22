d627b27b10dfea78f92212eec458dcd90369f6dc
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2024-02-22 06:31:19 -0500

    11 commit

---
+++ pages/page6/page6.js
@@ -0,0 +1,14 @@
+document.getElementById('backButton').addEventListener('click', function() {
+    // Go to the previous page
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page5/page5.html';
+});
+
+document.getElementById('homeButton').addEventListener('click', function() {
+    // Go to the home page - replace with your actual home page URL or path
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/';
+});
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }

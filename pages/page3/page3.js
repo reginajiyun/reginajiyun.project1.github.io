cbee8e7621897c0a247718e90ed8addf4e8ebb8c
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2024-02-22 03:04:07 -0500

    3 commit

---
+++ pages/page3/page3.js
@@ -0,0 +1,14 @@
+document.getElementById('backButton').addEventListener('click', function() {
+    // Go to the previous page
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page2/page2.html';
+});
+
+document.getElementById('nextButton').addEventListener('click', function() {
+    // Go to the next page - replace with your actual next page URL or path
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page4/page4.html';
+});
+
+document.getElementById('homeButton').addEventListener('click', function() {
+    // Go to the home page - replace with your actual home page URL or path
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/';
+});

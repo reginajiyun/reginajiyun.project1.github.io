3d590549a2aac1829311cf797aa166d3736616bc
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2024-02-22 02:15:05 -0500

    2 commit

--- pages/page2/page2.js
+++ pages/page2/page2.js
@@ -1,11 +1,11 @@
 document.getElementById('backButton').addEventListener('click', function() {
     // Go to the previous page
-    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/';
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page1/page1.html';
 });
 
 document.getElementById('nextButton').addEventListener('click', function() {
     // Go to the next page - replace with your actual next page URL or path
-    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page2/page2.html';
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page3/page3.html';
 });
 
 document.getElementById('homeButton').addEventListener('click', function() {

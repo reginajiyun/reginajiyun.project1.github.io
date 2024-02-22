cf95b3c5096fcc09d703a2c530c35adcc9f9d911
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2024-02-22 02:11:29 -0500

    1 commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('nextButton').addEventListener('click', function() {
     // Replace 'nextPage.html' with the actual URL or path to your next page
-    window.location.href = '';
+    window.location.href = 'https://reginajiyun.github.io/reginajiyun.project1.github.io/pages/page1/page1.html';
 });
 

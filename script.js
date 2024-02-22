e866e092bd59082d198cc2937d63906421635658
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2024-02-22 01:46:09 -0500

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('nextButton').addEventListener('click', function() {
+    // Replace 'nextPage.html' with the actual URL or path to your next page
+    window.location.href = '';
+});
+
